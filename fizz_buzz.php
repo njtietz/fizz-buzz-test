4dd76f8684c7100a3cc0a9bee37f3f66fb474208
Nick <njtietz@gmail.com>
2016-12-28 16:45:12 -0500

    fizz_buzz.php draft

---
+++ fizz_buzz.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Fizz Buzz:
+Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”. This can be done in either PHP or Javascript.
+*/
+
+class fizz_buzz{
+
+	public function print_1_to_100(){
+		for ($i=0; $i <= 100; $i++) { 
+			$value = $this->get_fizz_buzz_value($i);
+			echo $value.PHP_EOL;
+		}
+	}
+
+	private function get_fizz_buzz_value($input){
+		if($input % 3 == 0 && $input % 5 == 0){
+			return "fizzbuzz";
+		} else if ($input % 3 == 0){
+			return "fizz";
+		} else if ($input % 5 == 0){
+			return "buzz";
+		} else {
+			return $input;
+		}
+	}
+
+}
+
+$fizz_buzz = new fizz_buzz();
+$fizz_buzz->print_1_to_100();
